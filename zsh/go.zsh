adaea1a1921aff036c1558ff67e777c1ef926794
Thomas Coussot <thomas.coussot@diateam.net>
2021-07-09 10:43:53 +0200

    feat: add unvendor func for go mod

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -3,3 +3,33 @@ export GOPROXY="direct"
 
 alias ppm='go run main.go |&pp'
 alias -g PP=' |&pp'
+
+unvendor() {
+	project=$1
+	gopath="${GOPATH:=$HOME/go}/src"
+	echo $gopath/$project
+
+	if [[ $project == "" ]]; then
+		echo "No project given. Usage: $0 <library>"
+		return 1
+	fi
+
+	if ! test -d ./vendor; then
+		echo "No vendor found! Exiting"
+		return 1
+	fi
+
+	if ! test -d ./vendor/$project; then
+		echo "vendor library $project not found. Exiting"
+		return 1
+	fi
+
+	if ! test -d $gopath/$project; then
+		echo "library $project not found in go src. Exiting"
+		return 1
+	fi
+
+	/bin/rm -rf "./vendor/$project"
+	ln -s $gopath/$project "./vendor/$project" 
+}
+
